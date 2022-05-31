99b6220ebb617e154f345dc6e4eea9769d1f4a51
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 23:09:37 +0530

    Update sscce-helpers.js

--- dev/sscce-helpers.js
+++ dev/sscce-helpers.js
@@ -4,7 +4,7 @@ const Support = require('../test/support');
  
 module.exports = {
   createSequelizeInstance(options = {}) { 
-    return Support.createSequelizeInstance({ 
+    return Support.createSequelizeInstance({  
       logging: console.log,
       logQueryParameters: true,
       ...options

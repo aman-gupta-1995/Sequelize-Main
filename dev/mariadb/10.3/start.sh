f8023ee32dcd338a1e05a46bef59038c296f4f05
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 23:12:53 +0530

    Update start.sh

--- dev/mariadb/10.3/start.sh
+++ dev/mariadb/10.3/start.sh
@@ -2,7 +2,7 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
  
- 
+  
 docker-compose -p sequelize-mariadb-103 down --remove-orphans
 docker-compose -p sequelize-mariadb-103 up -d 
 

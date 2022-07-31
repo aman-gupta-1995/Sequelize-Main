1d8a366767585ce3038ae00b221715d2f41b40b3
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-31 11:17:05 +0530

    Update sscce.js

--- sscce.js
+++ sscce.js
@@ -6,7 +6,7 @@ const { createSequelizeInstance } = require('./dev/sscce-helpers');
 const { Model, DataTypes } = require('.');   
     
 const { expect } = require('chai'); // You can use `expect` on your SSCCE!   
- 
+  
 const sequelize = createSequelizeInstance({ benchmark: true }); 
 
 class User extends Model {}

4f75aa13c74bf754a80108a676b55192a60ef525
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-06 19:13:27 +0530

    Update count.ts

--- types/test/count.ts
+++ types/test/count.ts
@@ -2,7 +2,7 @@ import { expectTypeOf } from "expect-type";
 import { Model, Op } from 'sequelize';
 
 class MyModel extends Model {}
-  
+   
 expectTypeOf(MyModel.count()).toEqualTypeOf<Promise<number>>();        
 expectTypeOf(MyModel.count({ group: 'tag' })).toEqualTypeOf<Promise<{ [key: string]: number }>>();       
 expectTypeOf(MyModel.count({ col: 'tag', distinct: true })).toEqualTypeOf<Promise<number>>();   
